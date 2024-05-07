8982d7d078b4feb60114dc3752ee9498cbef4133
Vladimir <168404596+Daemonicola@users.noreply.github.com>
2024-05-07 12:49:09 -0700

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,10 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+point_1 = sum(grades[0])/len(grades[0])
+print(point_1)
+point_2 = sum(grades[1])/len(grades[1])
+point_3 = sum(grades[2])/len(grades[2])
+point_4 = sum(grades[3])/len(grades[3])
+point_5 = sum(grades[4])/len(grades[4])
+journal = {'Aaron':point_1,'Bilbo':point_2,'Johnny':point_3,'Khendrik':point_4,'Steve':point_5 }
+print(journal)
